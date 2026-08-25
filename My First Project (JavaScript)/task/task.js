3a2e46fc6df20f85cefe770f5b0b127e2b0073e0
jaesu968 <mazterk968@gmail.com>
2026-08-25 13:51:51 -0500

    Measure the total income of your shop - stage 2 complete

--- My First Project (JavaScript)/task/task.js
+++ My First Project (JavaScript)/task/task.js
@@ -7,7 +7,7 @@ const input = require('sync-input');
 // let age = Number(input("Type your age: "));
 // You will need it in later stages.
 
-
+// initial prices of products
 const products = {
     Bubblegum : 2,
     Toffee: 0.2,
@@ -16,8 +16,23 @@ const products = {
     Doughnut: 2.5,
     Pancake: 3.2
 }
-// print each product with its price
-console.log("Prices:")
-for (const [product, price] of Object.entries(products)) {
-    console.log(`${product}; $${price}`);
+const sales = {
+    Bubblegum : 202,
+    Toffee: 118,
+    "Ice cream": 2250,
+    "Milk chocolate": 1680,
+    Doughnut: 1075,
+    Pancake: 80
 }
+// print each product with its price
+console.log("Earned amount:")
+for (const [product, saleAmount] of Object.entries(sales)) {
+    console.log(`${product}: $${saleAmount}`);
+}
+// variable to get sum of sales
+let sum = 0;
+for(let item in sales){
+    sum += sales[item];
+}
+console.log("");
+console.log(`Income: $${sum}`);
